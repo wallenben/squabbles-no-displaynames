92fd341f2085fd3b9e951be40d00496241dabc47
Ben Wallen <72104872+wallenben@users.noreply.github.com>
2023-06-11 17:51:28 -0700

    Upload script

---
+++ script.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Remove Display Names
+// @namespace     https://squabbles.io/
+// @version      0.1
+// @description  Removes all display names from squabbles.
+// @author       pbjpie / rayquaza
+// @match        https://squabbles.io/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict'; 
+    function removeElements(elements) {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].parentNode.removeChild(elements[i]);
+        }
+    }
+    function checkForElements() {
+        let elements = document.getElementsByClassName('text-decoration-none text-muted');
+        if(elements.length > 0){
+            removeElements(elements);
+        }
+    }
+    setInterval(checkForElements, 1000);
+})();
